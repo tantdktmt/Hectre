7be6076ee33f8b8f8c57b063a250763161ff0a67
Tran Duc Tan <tan.tranduc@ascendcorp.com>
2022-04-10 11:09:39 +0700

    Add toolbar widget

---
+++ extension/src/main/java/com/hectre/extension/ViewExt.kt
@@ -0,0 +1,26 @@
+package com.hectre.extension
+
+import android.os.SystemClock
+import android.view.View
+
+fun View.setSafeOnClickListener(onSafeClick: (View) -> Unit) {
+    val safeClickListener = SafeClickListener {
+        onSafeClick(it)
+    }
+    setOnClickListener(safeClickListener)
+}
+
+class SafeClickListener(
+    private var defaultInterval: Int = 1000,
+    private val onSafeCLick: (View) -> Unit
+) : View.OnClickListener {
+    private var lastTimeClicked: Long = 0
+
+    override fun onClick(v: View) {
+        if (SystemClock.elapsedRealtime() - lastTimeClicked < defaultInterval) {
+            return
+        }
+        lastTimeClicked = SystemClock.elapsedRealtime()
+        onSafeCLick(v)
+    }
+}
