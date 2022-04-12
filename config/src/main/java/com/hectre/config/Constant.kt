11a62b694f0e64c52a34bd5bd46a050430eca10d
Tran Duc Tan <tan.tranduc@ascendcorp.com>
2022-04-12 15:28:46 +0700

    Update confirm button

--- config/src/main/java/com/hectre/config/Constant.kt
+++ config/src/main/java/com/hectre/config/Constant.kt
@@ -3,5 +3,5 @@ package com.hectre.config
 object Constant {
 
     const val DEBUG_LOG_TAG = "HectreApp"
-    const val API_BASE_URL = "http://alt-apin.biznetiptv.com:18080/api/"
+    const val API_BASE_URL = "http://baseurl/"
 }
